ffe5516c2416a10e9ab1c427c649d51ac4ef7ef5
iwzy7071 <iwzy7071@gmail.com>
2020-07-03 13:43:48 +0800

    完成注册中心、Sentinel、网关、流量监控

---
+++ user/src/main/java/com/search/UserApplication.java
@@ -0,0 +1,32 @@
+package com.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+
+        @RequestMapping(value = "/login")
+        public String login() {
+            return "Hello Nacos Discovery";
+        }
+
+    }
+
+}
---
+++ user/src/main/java/com/search/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.search.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+}
